c6d991d5f8434192295f930179d6ed2e7c7d9f04
manik <maniksarker652@gmail.com>
2024-09-07 09:09:16 +0600

    normalize the conversation and message collection and implement image functionality for chatting

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -147,6 +147,23 @@ export default function MessageBox() {
         </div>
         <div className="overflow-x-hidden overflow-y-scroll h-[80vh]">
           <div className="flex flex-col gap-2 py-2 mx-2" ref={currentMessage}>
+            {/* {allMessage.map((msg, index) => {
+              return (
+                <div
+                  key={index}
+                  className={` p-1 py-1 rounded w-fit max-w-[280px] md:max-w-sm lg:max-w-md ${
+                    decodedData?.userId === msg?.msgByUserId
+                      ? "ml-auto bg-teal-100"
+                      : "bg-white"
+                  }`}
+                >
+                  <p className="px-2">{msg.text}</p>
+                  <p className="text-xs ml-auto w-fit">
+                    {moment(msg.createdAt).format("hh:mm")}
+                  </p>
+                </div>
+              );
+            })} */}
             {allMessage.map((msg, index) => {
               return (
                 <div
@@ -157,7 +174,21 @@ export default function MessageBox() {
                       : "bg-white"
                   }`}
                 >
-                  {/* <div className="w-full relative"></div> */}
+                  <div className="w-full relative">
+                    {msg?.imageUrl && (
+                      <img
+                        src={msg?.imageUrl}
+                        className="w-full h-full object-scale-down"
+                      />
+                    )}
+                    {msg?.videoUrl && (
+                      <video
+                        src={msg.videoUrl}
+                        className="w-full h-full object-scale-down"
+                        controls
+                      />
+                    )}
+                  </div>
                   <p className="px-2">{msg.text}</p>
                   <p className="text-xs ml-auto w-fit">
                     {moment(msg.createdAt).format("hh:mm")}
@@ -176,7 +207,7 @@ export default function MessageBox() {
               </div>
               <div className="bg-white p-3">
                 <img
-                  src={message.imageUrl}
+                  src={newMessage?.imageUrl}
                   alt="uploadImage"
                   className="aspect-square w-full h-full max-w-sm m-2 object-scale-down"
                 />
